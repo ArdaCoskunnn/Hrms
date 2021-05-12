2ba872d28a65753f051f1293de266adde36e13cc
ArdaCoskun <68504085+ArdaCoskun@users.noreply.github.com>
2021-05-12 12:03:43 +0300

    Hrms

---
+++ hrms/src/main/java/kodlama/io/hrms/api/controllers/JobPositionsController.java
@@ -0,0 +1,29 @@
+package kodlama.io.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.hrms.business.abstracts.JobPositionService;
+import kodlama.io.hrms.entities.concretes.JobPosition;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobPositions")
+public class JobPositionsController {
+	
+	private JobPositionService jobPositionService;
+	
+	@Autowired
+	public JobPositionsController(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+	
+	@GetMapping("/getall")
+	public List<JobPosition> getAll(){
+		return this.jobPositionService.getAll();
+	}
+}
